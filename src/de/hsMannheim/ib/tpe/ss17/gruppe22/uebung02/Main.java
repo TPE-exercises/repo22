bc726eeb76487b4db0531485048fc55eab1c892b
MarPra <marco.96p@hotmail.de>
2017-04-24 15:23:14 +0200

    Update Main.java

--- src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung02/Main.java
+++ src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung02/Main.java
@@ -1,13 +1,19 @@
 package de.hsMannheim.ib.tpe.ss17.gruppe22.uebung02;
 
+
 public class Main {
 
 	  public static void main(String[] args) {
 	    // TODO Auto-generated method stub
-	    CrypterCaesar myCrypter = new CrypterCaesar(3);
-	    String myString = "ich, julius caesar, sagte den folgenden beruehmten satz: veni, vidi, vici.";
+	    CrypterCaesar myCrypter = new CrypterCaesar((26*3000)*(-1)+1);
+	    String myString = "K";
+//	    String myString = "Ich, Julius Caesar, sagte den folgenden beruehmten Satz: Veni, vidi, vici.";
 	    String myEncryptedString = myCrypter.encrypt(myString);
+	    System.out.print("Encrypted Message: ");
 	    System.out.println(myEncryptedString);
+	    String myDecryptedString = myCrypter.decrypt(myEncryptedString);
+	    System.out.print("Decrypted Message: ");
+	    System.out.println(myDecryptedString);
 	  }
 
 	}
