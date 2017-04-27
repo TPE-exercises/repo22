98b6a9ac4eb78044afad58c583ad131d83ea20a0
Marco <Marco@Jaqueline>
2017-04-27 14:33:32 +0200

    Replaced 26 by this.alphabet.length in method decrypt().

--- src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung02/Main.java
+++ src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung02/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
 	  public static void main(String[] args) {
 	    // TODO Auto-generated method stub
-	    CrypterCaesar myCrypter = new CrypterCaesar((26*3000)*(-1)+1);
-	    String myString = "K";
-//	    String myString = "Ich, Julius Caesar, sagte den folgenden beruehmten Satz: Veni, vidi, vici.";
+	    Crypter myCrypter = new CrypterCaesar((26*3000)*(-1)+1);
+//	    String myString = "K";
+	    String myString = "Ich, Julius Caesar, sagte den folgenden beruehmten Satz: Veni, vidi, vici.";
 	    String myEncryptedString = myCrypter.encrypt(myString);
 	    System.out.print("Encrypted Message: ");
 	    System.out.println(myEncryptedString);
