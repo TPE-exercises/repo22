9546f4d768eaaae6f3bf4c2807e2986dbcbc4a99
MarPra <marco.96p@hotmail.de>
2017-05-01 17:36:56 +0200

    Delete Sortable.java

--- Sortable.java
+++
@@ -1,8 +0,0 @@
-package de.hsMannheim.ib.tpe.ss17.gruppe22.uebung02;
-
-public interface Sortable {
-	public static void sortArray(Sortable sortierverfahren, Comparable[] array) {
-		sortierverfahren.sortArray(array);
-	}
-	public void sortArray(Comparable[] array);
-}
