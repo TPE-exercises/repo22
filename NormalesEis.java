875eebbde2a1bdd6b0665a18a20e0ee0129ce5ba
MarPra <marco.96p@hotmail.de>
2017-05-01 17:36:40 +0200

    Delete NormalesEis.java

--- NormalesEis.java
+++
@@ -1,8 +0,0 @@
-package de.hsMannheim.ib.tpe.ss17.gruppe22.uebung02;
-
-public class NormalesEis extends Eis {
-	public NormalesEis(String name, String behaeltnis, String art, String sorten[], String extras[], double preis){
-		super (name,behaeltnis,art, sorten, extras, preis);
-	}
-
-}
