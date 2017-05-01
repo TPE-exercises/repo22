90c54d8ce8b579786812f4601bf6301fc2b95533
MarPra <marco.96p@hotmail.de>
2017-05-01 17:37:04 +0200

    Delete StandardEisdiele.java

--- StandardEisdiele.java
+++
@@ -1,9 +0,0 @@
-package de.hsMannheim.ib.tpe.ss17.gruppe22.uebung02;
-
-public class StandardEisdiele extends Eisdiele{
-	public StandardEisdiele(){
-	}
-	public Eis erstellen(String typ){
-		return bestellen(typ);
-	}
-}
