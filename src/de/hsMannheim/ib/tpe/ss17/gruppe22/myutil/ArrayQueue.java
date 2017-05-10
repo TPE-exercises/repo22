3566462ea73ec0dda9155e68b29aa254cbaefaac
MarPra <marco.96p@hotmail.de>
2017-05-10 02:34:48 +0200

    Update ArrayQueue.java
    Changed a lot

--- src/de/hsMannheim/ib/tpe/ss17/gruppe22/myutil/ArrayQueue.java
+++ src/de/hsMannheim/ib/tpe/ss17/gruppe22/myutil/ArrayQueue.java
@@ -2,11 +2,27 @@ package de.hsMannheim.ib.tpe.ss17.gruppe22.myutil;
 
 public class ArrayQueue implements Queue {
 
-    Object[] q;
+    private Object[] q;
     private int front = 0;
     private int end = 0;
     private int count = 0;
     private boolean expandable = true;
+
+    public int getFront() {
+        return this.front;
+    }
+
+    public int getEnd() {
+        return this.end;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public void showInformation() {
+        System.out.println("Front: " + getFront() + "; End: " + getEnd() + "; Count: " + getCount());
+    }
 
     public ArrayQueue() {
         this.q = new Object[16384];
@@ -29,38 +45,45 @@ public class ArrayQueue implements Queue {
     @Override
     public void enter(Object el) {
         if (!isFull()) {
+            if (this.end == (this.q.length)) {
+                this.end = 0;
+            }
             this.q[this.end] = el;
             this.count++;
 
-            if (this.end == (this.q.length - 1)) {
-                this.end = 0;
-            } else {
-                this.end++;
-            }
+            this.end++;
 
-        }
-
-        if (!expandable) { // Queue is full if this is asked
-            System.out.println("Queue is full. We are sorry.");
-        } else {
+        } else if (expandable) {
             expand();
+            enter(el);
+        } else {
+            System.out.println("Queue is full. We are sorry.");
         }
     }
 
     public void expand() {
         Object temp[] = new Object[this.q.length * 2];
+        int tempCount = this.count;
+        int tempFront = this.front;
         if (this.front != 0) {
-            for (int i = this.front; i != 0; i++) {
-                temp[this.q.length - this.front] = leave();
+            /**
+             * TODO: Implement Correctly / Correct this
+             */
+            for (int i = this.front; i <= this.q.length; i++) {
+                temp[temp.length - this.q.length + this.front] = leave();
             }
+            this.front = tempFront + this.q.length;
         } else {
             for (int i = 0; !isEmpty(); i++) {
                 temp[i] = leave();
             }
+            this.front = 0;
         }
+        this.count = tempCount;
+
         this.q = temp;
-        expandable = false;
-        System.out.println("Die Queue wurde erfolgreich vergrößert.");
+        this.expandable = false;
+        System.out.println("Die Queue wurde expandiert.");
     }
 
     /**
@@ -69,23 +92,30 @@ public class ArrayQueue implements Queue {
      * @return the element at the front of the queue.
      */
     @Override
-    public Object leave() throws NullPointerException {
+    public Object leave() {
+        Object temp;
         try {
-            if (!isEmpty()) {
-                this.count--;
-                if (this.end != (this.front)) {
-                    this.front += 1;
-                    if (this.front >= this.q.length) {
-                        this.front = 0;
-                    }
-                }
-                return this.q[this.front];
-            } else {
-                throw new NullPointerException();
-            }
+            temp = GiveFront();
         } catch (NullPointerException e) {
-            System.out.println("leave() kann nicht auf eine leere Queue angewandt werden!");
+            System.out.println("**Fehler: leave() kann nicht auf eine leere Queue angewandt werden!**");
             return null;
+        }
+        if (!isEmpty()) {
+
+            this.front += 1;
+            if (this.front >= this.q.length) {
+                this.front = 0;
+            }
+            this.count--;
+        }
+        return temp;
+    }
+
+    public Object GiveFront() throws NullPointerException {
+        if (!isEmpty()) {
+            return this.q[this.front];
+        } else {
+            throw new NullPointerException();
         }
     }
 
@@ -112,3 +142,4 @@ public class ArrayQueue implements Queue {
         return this.count;
     }
 }
+
