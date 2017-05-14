d7213c85a191844cc3c61f4e7e2aa4534b0f04aa
MarPra <marco.96p@hotmail.de>
2017-05-14 17:56:23 +0200

    Update CaesarReader.java
    Commented out a console print.

--- src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung03/CaesarReader.java
+++ src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung03/CaesarReader.java
@@ -1,70 +1,116 @@
 package de.hsMannheim.ib.tpe.ss17.gruppe22.uebung03;
 
 import de.hsMannheim.ib.tpe.ss17.gruppe22.uebung02.CrypterCaesar;
-import java.io.FilterReader;
+import java.io.FilterWriter;
 import java.io.IOException;
-import java.io.Reader;
+import java.io.Writer;
 
-public class CaesarReader extends FilterReader {
+// import de.hsMannheim.ib.tpe.ss17.gruppe22.uebung02.CrypterCaesar;
+public class CaesarWriter extends FilterWriter {
 
-	public char[] alphabet = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-			'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-			'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'Ä', 'Ö', 'Ü', 'ä', 'ö',
-			'ü' };
-	public CrypterCaesar cryptEngine = new CrypterCaesar();
-	protected Reader in;
+    public char[] alphabet = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'Ä', 'Ö', 'Ü', 'ä', 'ö',
+        'ü'};
+    public CrypterCaesar cryptEngine = new CrypterCaesar();
+    protected Writer out;
 
-	public CaesarReader(Reader in) {
-		super(in);
-		this.in = in;
-		this.cryptEngine.setAlphabet(this.alphabet);
-	}
+    /**
+     * Opens a new writer
+     *
+     * @param out the writer to be opened.
+     */
+    public CaesarWriter(Writer out) {
+        super(out);
+        this.out = out;
+        this.cryptEngine.setAlphabet(this.alphabet);
+    }
 
-	public CaesarReader(Reader in, int charShift) {
-		this(in);
-		this.cryptEngine.setCharShift(charShift);
-	}
-	/*
-	 * Reads and decrypts the Array cbuf from off to len
-	 * @see java.io.FilterReader#read(char[], int, int)
-	 */
-	@Override
-	public int read(char[] cbuf, int off, int len) throws IOException {
-		int charsToRead = in.read(cbuf, off, len);
-		if (len < off)
-			throw new IOException();
-		
-		for (int i = off; len > 0; i++){
-			String tempString = "";
-			tempString += cbuf[i];
-			cbuf[i] = this.cryptEngine.decrypt(tempString).charAt(0);
-			len--;
-		}
-		return charsToRead;
-	}
-	/*
-	 * Reads and decrypts the entire Array cbuf
-	 * decrypts in read(char, int, int)
-	 * @see java.io.Reader#read(char[])
-	 */
-	@Override
-	public int read(char[] cbuf) throws IOException {
-		return read(cbuf, 0, cbuf.length);
-	}
-	/*
-	 * Reads and decrypts a single char
-	 * @see java.io.FilterReader#read()
-	 */
-	@Override
-	public int read() throws IOException {
-		String tempString = "";
-		int lastRead;
-		lastRead = in.read();
-		if (lastRead == -1)
-			return -1;
-		tempString += (char) lastRead;
-		tempString = cryptEngine.decrypt(tempString);
-		return (int) tempString.charAt(0);
+    public CaesarWriter(Writer out, int charShift) {
+        this(out);
+        this.cryptEngine.setCharShift(charShift);
+    }
 
-	}
+    /**
+     * Encrypts a portion (off, len) of an array (cbuf) of characters and writes
+     * it. The encryption takes place in write(String).
+     *
+     * @throws java.io.IOException
+     */
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        char[] temp = new char[len];
+        int counter = 0;
+        for (int i = off; len > 0; i++) {
+            if (i >= cbuf.length) {
+                throw new IOException();
+            }
+            temp[counter] = cbuf[i];
+            counter++;
+            len--;
+        }
+        write(temp);
+    }
+
+    /**
+     * Encrypts a char array and writes it. The encryption takes place in
+     * write(String).
+     *
+     * @param cbuf the chars to be encrypted
+     * @throws IOException
+     */
+    @Override
+    public void write(char[] cbuf) throws IOException {
+        for (int i = 0; i < cbuf.length; i++) {
+            write((int) cbuf[i]);
+        }
+    }
+
+    /**
+     * Encrypts a single character and writes it. The encryption takes place in
+     * write(String).
+     *
+     * @param c the char to be encrypted
+     * @throws java.io.IOException
+     */
+    @Override
+    public void write(int c) throws IOException {
+        char temp = (char) c;
+        String tempString = "" + temp;
+        write(tempString);
+    }
+
+    /**
+     * Encrypts a portion of a string and writes it. The encryption takes place
+     * in write(String).
+     *
+     * @param str the string to be encrypted
+     * @param off the offset point
+     * @throws java.io.IOException
+     */
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        String tempString = "";
+        for (int i = off; len > 0; i++) {
+            if (i > str.length()) {
+                throw new IOException();
+            }
+            tempString = tempString + str.charAt(i);
+            len--;
+        }
+        write(tempString);
+    }
+
+    /**
+     * Encrypts a string using Caesar encryption and writes it.
+     *
+     * @param str The string to be encrypted
+     * @throws IOException
+     */
+    @Override
+    public void write(String str) throws IOException {
+        str = cryptEngine.encrypt(str);
+        out.write(str);
+//        System.out.println(str + "\n wurde in die Datei geschrieben.");
+    }
 }
