382d3cc9b4bb5bab91afb2ae2cd5e90e904e1b66
MarPra <marco.96p@hotmail.de>
2017-05-27 14:05:22 +0200

    Update BankAccountThread.java

--- src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/BankAccountThread.java
+++ src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/BankAccountThread.java
@@ -3,8 +3,7 @@
 package de.hsMannheim.ib.tpe.ss17.gruppe22.uebung04;
 
 /**
- *
- * @author Marco
+ * Creates a new bank account with a starting balance of 1000€.
  */
 public class BankAccountThread extends Thread {
     public static BankAccount myBankAccount = BankAccount.createNewBankAccount(1000);
