8528ca061a7b918b360f2137fc98318ef71fbd80
MarPra <marco.96p@hotmail.de>
2017-05-28 18:18:13 +0200

    Create SortAlgorithm.java

---
+++ src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/SortAlgorithm.java
@@ -0,0 +1,15 @@
+/*
+ */
+package de.hsMannheim.ib.tpe.ss17.gruppe22.uebung04;
+
+/**
+ *
+ * @author Marco
+ */
+public interface SortAlgorithm {
+    /**
+     * Sorts an array.
+     * @param array the array to be sorted.
+     */
+    public void sort(Comparable[]array);
+}
