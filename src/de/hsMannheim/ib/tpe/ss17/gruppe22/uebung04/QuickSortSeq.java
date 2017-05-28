bcc7def658d70e87f42e5df5de97e38348b05b13
mmnn11 <marcmehrer@t-online.de>
2017-05-29 00:02:37 +0200

    Update QuickSortSeq.java

--- src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/QuickSortSeq.java
+++ src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/QuickSortSeq.java
@@ -7,8 +7,5 @@ package de.hsMannheim.ib.tpe.ss17.gruppe22.uebung04;
  * @author Marco
  */
 public class QuickSortSeq extends QuickSort {
-
-    @Override
-    public void sort(Comparable[]) {
-    }
+    
 }
