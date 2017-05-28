00a31660fa24435e9856cd7d37da23c999538516
MarPra <marco.96p@hotmail.de>
2017-05-28 21:00:42 +0200

    Create QuickSortSeq.java

---
+++ src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/QuickSortSeq.java
@@ -0,0 +1,14 @@
+/*
+ */
+package de.hsMannheim.ib.tpe.ss17.gruppe22.uebung04;
+
+/**
+ *
+ * @author Marco
+ */
+public class QuickSortSeq extends QuickSort {
+
+    @Override
+    public void sort(Comparable[]) {
+    }
+}
