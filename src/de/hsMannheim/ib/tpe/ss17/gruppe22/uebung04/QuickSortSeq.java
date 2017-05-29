2cbeef076dfb6e83afee4a890c96bce5fa4aae23
MarPra <marco.96p@hotmail.de>
2017-05-29 14:34:35 +0200

    Update QuickSortSeq.java

--- src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/QuickSortSeq.java
+++ src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/QuickSortSeq.java
@@ -2,10 +2,12 @@
  */
 package de.hsMannheim.ib.tpe.ss17.gruppe22.uebung04;
 
-/**
- *
- * @author Marco
- */
 public class QuickSortSeq extends QuickSort {
-    
+	
+
+	@Override
+	public void sort(Comparable[] array) {
+		quickSort(array, 0, array.length - 1);
+		
+	}
 }
