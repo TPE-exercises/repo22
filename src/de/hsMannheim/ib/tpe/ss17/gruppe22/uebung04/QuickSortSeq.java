6a9a664de909a1dfb6bbb72089819d26903de072
MarPra <marco.96p@hotmail.de>
2017-06-05 20:43:21 +0200

    Updated comments

--- src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/QuickSortSeq.java
+++ src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/QuickSortSeq.java
@@ -3,11 +3,15 @@
 package de.hsMannheim.ib.tpe.ss17.gruppe22.uebung04;
 
 public class QuickSortSeq extends QuickSort {
-	
 
-	@Override
-	public void sort(Comparable[] array) {
-		quickSort(array, 0, array.length - 1);
-		
-	}
+    /**
+     * Sorts an array using quick sort.
+     *
+     * @param array the array to be sorted.
+     */
+    @Override
+    public void sort(Comparable[] array) {
+        quickSort(array, 0, array.length - 1);
+
+    }
 }
