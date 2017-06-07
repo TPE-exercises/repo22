b818132b4095fc5fb07513738d47501dc626333f
MarPra <marco.96p@hotmail.de>
2017-06-08 00:05:25 +0200

    Update ConsumerThread.java

--- src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/ConsumerThread.java
+++ src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/ConsumerThread.java
@@ -13,36 +13,38 @@ public class ConsumerThread extends Thread {
 
     Ringpuffer consumer;
     int sleepTime = 2000;
+
     ConsumerThread(Ringpuffer newConsumer) {
         super();
         this.consumer = newConsumer;
     }
+
     ConsumerThread(Ringpuffer newConsumer, int sleepTime) {
         this(newConsumer);
         this.sleepTime = sleepTime;
     }
+
     /**
-     * sleeps X milliseconds, then consumes.
-     * X by default is 2000.
+     * consumes, then sleeps X milliseconds, then repeat. X by default is 2000.
      */
     @Override
     public void run() {
         while (!TimerThread.timeIsOver()) {
+            consume();
             try {
                 sleep(sleepTime);
             } catch (InterruptedException ex) {
                 Logger.getLogger(ConsumerThread.class.getName()).log(Level.SEVERE, null, ex);
             }
-            consume();
         }
     }
- 
+
     /**
      * Tries to get an element from 'Ringpuffer' and prints it on the console.
      */
     public void consume() {
         synchronized (this) {
-            System.out.println("Ich nehme das Element " + consumer.get() + " aus dem Ringpuffer.");
+            System.out.println("Ich, " + this + " nehme das Element " + consumer.get() + " aus dem Ringpuffer.");
         }
     }
 }
