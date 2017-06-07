3e7025c7be9597dd36e0a732f1bc7d4e82fb5a12
MarPra <marco.96p@hotmail.de>
2017-06-08 00:05:40 +0200

    Update ProducerThread.java

--- src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/ProducerThread.java
+++ src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung04/ProducerThread.java
@@ -2,10 +2,52 @@
  */
 package de.hsMannheim.ib.tpe.ss17.gruppe22.uebung04;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Marco
  */
 public class ProducerThread extends Thread {
-    
+
+    Ringpuffer producer;
+    int sleepTime = 5000;
+
+    ProducerThread(Ringpuffer newProducer) {
+        super();
+        this.producer = newProducer;
+    }
+
+    ProducerThread(Ringpuffer newProducer, int sleepTime) {
+        this(newProducer);
+        this.sleepTime = sleepTime;
+    }
+
+    /**
+     * produces, then sleeps X milliseconds, then repeat. X by default is 5000.
+     */
+    @Override
+    public void run() {
+        while (!TimerThread.timeIsOver()) {
+            produce();
+            try {
+                sleep(sleepTime);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ConsumerThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+
+    /**
+     * Tries to get an element from 'Ringpuffer' and prints it on the console.
+     */
+    public void produce() {
+        synchronized (this) {
+            Object product = (int) (Math.random() * 200);
+            producer.put(product);
+            System.out.println("Ich, " + this + " füge die Zahl " + product + " ein.");
+        }
+    }
 }
