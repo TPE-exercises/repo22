eebddf8b7ed6d6b9f5927db4ffe4b8c9df477048
MarPra <marco.96p@hotmail.de>
2017-06-18 17:06:58 +0200

    Update WordCount.java

--- src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung05/WordCount.java
+++ src/de/hsMannheim/ib/tpe/ss17/gruppe22/uebung05/WordCount.java
@@ -1,75 +1,147 @@
-package de.hsMannheim.ib.tpe.ss17.gruppe22.uebung05;
+package de.hsMannheim.ib.tpe.ss17.gruppe23.uebung05;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.math.BigInteger;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class WordCount {
+public final class WordCount {
 
-    HashMap wordMap;
-    BufferedReader fileReader;
+    Hashtable hashtable;
+    BufferedReader bufferedReader;
     // 4286435 is the length of the Bible.txt, 1178962 is the lenght of shakespeare.txt
     char[] textCharacters = new char[4286435];
+    private char alphabet[] = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'Ä', 'Ö', 'Ü', 'ä', 'ö',
+        'ü'};
 
     public WordCount(String fileToBeAnalyzed) throws IOException {
-        this.wordMap = new HashMap<String, Integer>(65536);
         try {
-            this.fileReader = new BufferedReader(new FileReader(fileToBeAnalyzed));
-            fileReader.read(this.textCharacters);
+            this.bufferedReader = new BufferedReader(new FileReader(fileToBeAnalyzed));
+            bufferedReader.read(this.textCharacters);
+            this.hashtable = new Hashtable<String, Integer>();
+            extractWords();
+            listWordsByFrequencyDescending();
+            closeReader();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(WordCount.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public void closeReader() {
+    /**
+     * Closes the reader which was opened upon initalizing an object of this
+     * class WordCount.
+     */
+    protected void closeReader() {
         try {
-            fileReader.close();
+            bufferedReader.close();
         } catch (IOException ex) {
             Logger.getLogger(WordCount.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     /**
-     * Gets the length of the currently inserted textfile.
+     * Reads a text file and spots each single word in there. Words are Strings,
+     * that only consist of letters. These words will be saved into a hashtable.
      *
-     * @return returns the length of the textfile
+     * Here, Words are not case sensitive, they will all be treated as if they
+     * were written with small letters only.
      */
-    public BigInteger getTextLength() {
-        BigInteger sum = BigInteger.valueOf(0);
-
+    public void extractWords() {
         try {
-            this.fileReader.mark(0);
-            // Calculates the file length.
-            while (fileReader.read() != -1) {
-                sum = sum.add(BigInteger.valueOf(1));
+            //System.out.println's are there for obversation purposes
+            System.out.println("bufferedReader.ready() " + bufferedReader.ready());
+            while (bufferedReader.ready()) {
+                String line = bufferedReader.readLine();
+                System.out.println("bufferedReader.readLine()" + line);
+                if (line != null) {
+                    extractWordsFromLine(line);
+                }
             }
-
-            this.fileReader.reset();
         } catch (IOException ex) {
             Logger.getLogger(WordCount.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return sum;
     }
 
-//    public void extractWords(char[] textCharacters) {
-//        int index = 0;
-//        String placeHolder = "";
-//        while (index < textCharacters.length) {
-//
-//            // if the next sign of the textCharacters cannot be part of a word, it must recognize that the end of the word has been reached
-//            if (textCharacters[index] == ' ' || textCharacters[index] == '.' || textCharacters[index] == ';' || textCharacters[index] == ':' || textCharacters[index] == '!' || textCharacters[index] == '?') {
-//                wordMap.put(placeHolder);
-//            }
-//            //if (textCharacters[index] == '-')
-//            placeHolder += textCharacters[index];
-//            index++;
-//        }
-//    }
+    /**
+     * Extracts the words from a text line and saves them into the hashtable of
+     * WordCount.
+     *
+     * Here, Words are not case sensitive, they will all be treated as if they
+     * were written with small letters only.
+     *
+     * @param line the line of text to be analyzed.
+     */
+    public void extractWordsFromLine(String line) {
+        int lineLength = line.length();
+        int lineIndex = 0;
+        boolean wordInExamination = true;
+        String nextWord = "";
+        while (lineIndex < lineLength && wordInExamination) {
+
+            int alphabetIndex = 0;
+            boolean charFound = false;
+            // Checks whether the current symbol is a letter from the alphabet.
+            while (alphabetIndex < this.alphabet.length && !charFound) {
+                if (line.charAt(lineIndex) == this.alphabet[alphabetIndex]) {
+                    nextWord += line.charAt(lineIndex);
+                    charFound = true;
+                }
+                alphabetIndex++;
+                // The current symbol is none of the alphabet
+                if (alphabetIndex == this.alphabet.length) {
+                    wordInExamination = false;
+                }
+            }
+
+            if (nextWord.length() != 0 && !wordInExamination) {
+                nextWord = nextWord.toLowerCase();
+                if (this.hashtable.get(nextWord) == null) {
+                    // inserts a word for its first time
+                    this.hashtable.put(nextWord, 1);
+                } else {
+                    // Increases the count of this word being found in the text
+                    Integer number = (Integer) this.hashtable.get(nextWord);
+                    number = number + 1;
+                    this.hashtable.put(nextWord, number);
+                }
+                nextWord = "";
+                wordInExamination = true;
+            }
+            lineIndex++;
+        }
+    }
+
+    /**
+     * Lists all the words currently saved in the hashtable of WordCount sorted
+     * by frequency, descending.
+     */
+    //TODO: Implement
+    public void listWordsByFrequencyDescending() {
+        Enumeration e = hashtable.keys();
+        Enumeration f = hashtable.elements();
+
+        while (e.hasMoreElements()) {
+            System.out.println(e.nextElement() + " " + f.nextElement());
+        }
+    }
+
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+        try {
+            WordCount wordcountExample = new WordCount("C:\\Users\\Marco\\Desktop\\Hochschule Mannheim\\TPE\\Übung 5 - Collections\\testfile.txt");
+//            wordcountExample.extractWords();
+//            wordcountExample.listWordsByFrequencyDescending();
+//            wordcountExample.closeReader();
+        } catch (IOException ex) {
+            Logger.getLogger(WordCount.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        time = System.currentTimeMillis() - time;
+        System.out.println("Benötigte Zeit (in ms): " + time);
+    }
 }
